12499d6ac3465bf684fae52b4c9a7b9207327dd7
Dan846 <danilam846@gmail.com>
2023-02-14 01:48:08 +0700

    123

--- src/components/card.js
+++ src/components/card.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 function Card({ image, ...props }) {
 	return (
 		<div className="card">
-			{image && <img src={image.src} className="card-img-top" alt={image.alt} />}
+			{image && <img src={image.src} alt={image.alt} />}
 			<div className="card-body">
 				{props.children}
 			</div>
